397a424362c1b0d0d14ba72d729c469f5203a58a
imuge <liuyuanmu@jd.com>
2021-07-02 15:58:16 +0800

    remove value in BytesValue json

---
+++ utils/base64/base64_test.go
@@ -0,0 +1,20 @@
+package base64
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestBase58Encode(t *testing.T) {
+	encode := Encode([]byte("abc"))
+	require.NotNil(t, encode)
+}
+
+func TestBase58Decode(t *testing.T) {
+	encode := Encode([]byte("abc"))
+	require.NotNil(t, encode)
+	decode, err := Decode(encode)
+	require.Nil(t, err)
+	require.NotNil(t, decode)
+	require.Equal(t, []byte("abc"), decode)
+}
