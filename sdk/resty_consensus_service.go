069a2bcd59a2cf2c35977a1a51be4b14a3eadad2
liuyuanmu <liuyuanmu@jd.com>
2022-05-05 17:58:21 +0800

    gmtls for management controller

--- sdk/resty_consensus_service.go
+++ sdk/resty_consensus_service.go
@@ -4,8 +4,13 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"github.com/blockchain-jd-com/framework-go/gmsm/gmtls"
+	gmx509 "github.com/blockchain-jd-com/framework-go/gmsm/x509"
 	"github.com/blockchain-jd-com/framework-go/ledger_model"
 	"github.com/go-resty/resty/v2"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
+	"net/url"
 	"strconv"
 )
 
@@ -17,11 +22,13 @@ import (
 var _ ledger_model.ParticipantService = (*RestyConsensusService)(nil)
 
 type RestyConsensusService struct {
-	host     string
-	port     int
-	secure   bool
-	baseUrl  string
-	security *SSLSecurity
+	host       string
+	port       int
+	secure     bool
+	baseUrl    string
+	security   *SSLSecurity
+	gmSecure   bool
+	gmSecurity *GMSSLSecurity
 }
 
 func NewRestyConsensusService(host string, port int) *RestyConsensusService {
@@ -45,6 +52,18 @@ func NewSecureRestyConsensusService(host string, port int, security *SSLSecurity
 	}
 }
 
+func NewGMSecureConsensusService(host string, port int, security *GMSSLSecurity) *RestyConsensusService {
+	baseUrl := fmt.Sprintf("https://%s:%d", host, port)
+	return &RestyConsensusService{
+		host:       host,
+		port:       port,
+		secure:     true,
+		gmSecure:   true,
+		baseUrl:    baseUrl,
+		gmSecurity: security,
+	}
+}
+
 func (r RestyConsensusService) ActivateParticipant(params ledger_model.ActivateParticipantParams) (info bool, err error) {
 	url := "/management/delegate/activeparticipant"
 	args := map[string]string{
@@ -58,6 +77,15 @@ func (r RestyConsensusService) ActivateParticipant(params ledger_model.ActivateP
 		"remoteManageSecure": strconv.FormatBool(params.RemoteManageSecure),
 		"shutdown":           strconv.FormatBool(params.Shutdown),
 	}
+
+	if !r.gmSecure {
+		return r.active(url, args)
+	} else {
+		return r.gmActive(url, args)
+	}
+}
+
+func (r RestyConsensusService) active(url string, args map[string]string) (info bool, err error) {
 	client := resty.New()
 	if r.secure {
 		if r.security != nil {
@@ -73,7 +101,6 @@ func (r RestyConsensusService) ActivateParticipant(params ledger_model.ActivateP
 	if err != nil {
 		return
 	}
-	fmt.Println(fmt.Sprintf("%s \n %v \n", url, resp))
 	if !resp.IsSuccess() {
 		return info, errors.New(resp.String())
 	}
@@ -85,12 +112,55 @@ func (r RestyConsensusService) ActivateParticipant(params ledger_model.ActivateP
 	}
 }
 
+func (r RestyConsensusService) gmActive(activeUrl string, args map[string]string) (info bool, err error) {
+	var certPool *gmx509.CertPool
+	if r.gmSecurity != nil {
+		certPool = r.gmSecurity.RootCerts
+	}
+	config := &gmtls.Config{
+		GMSupport:          &gmtls.GMSupport{},
+		RootCAs:            certPool,
+		ClientAuth:         gmtls.NoClientCert,
+		InsecureSkipVerify: r.security == nil || r.security.RootCerts == nil,
+	}
+
+	gmClient := gmtls.NewCustomHTTPSClient(config)
+	queryForm := url.Values{}
+	for k, v := range args {
+		queryForm.Add(k, v)
+	}
+	result, err := gmClient.PostForm(r.baseUrl+activeUrl, queryForm)
+	defer result.Body.Close()
+	if err != nil {
+		return
+	}
+	raw, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		return
+	}
+
+	tresp := gjson.ParseBytes(raw)
+	if !tresp.Get("success").Bool() {
+		return info, errors.New("active error")
+	} else {
+		return true, nil
+	}
+}
+
 func (r RestyConsensusService) InactivateParticipant(params ledger_model.InactivateParticipantParams) (info bool, err error) {
 	url := "/management/delegate/deactiveparticipant"
 	args := map[string]string{
 		"ledgerHash":         params.LedgerHash,
 		"participantAddress": params.ParticipantAddress,
 	}
+	if !r.gmSecure {
+		return r.inactive(url, args)
+	} else {
+		return r.gmInactive(url, args)
+	}
+}
+
+func (r RestyConsensusService) inactive(url string, args map[string]string) (info bool, err error) {
 	client := resty.New()
 	if r.secure {
 		if r.security != nil {
@@ -115,6 +185,41 @@ func (r RestyConsensusService) InactivateParticipant(params ledger_model.Inactiv
 		return info, errors.New("unparseable response")
 	} else {
 		return wrp.Success, nil
+	}
+}
+
+func (r RestyConsensusService) gmInactive(inactiveUrl string, args map[string]string) (info bool, err error) {
+	var certPool *gmx509.CertPool
+	if r.gmSecurity != nil {
+		certPool = r.gmSecurity.RootCerts
+	}
+	config := &gmtls.Config{
+		GMSupport:          &gmtls.GMSupport{},
+		RootCAs:            certPool,
+		ClientAuth:         gmtls.NoClientCert,
+		InsecureSkipVerify: r.security == nil || r.security.RootCerts == nil,
+	}
+
+	gmClient := gmtls.NewCustomHTTPSClient(config)
+	queryForm := url.Values{}
+	for k, v := range args {
+		queryForm.Add(k, v)
+	}
+	result, err := gmClient.PostForm(r.baseUrl+inactiveUrl, queryForm)
+	defer result.Body.Close()
+	if err != nil {
+		return
+	}
+	raw, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		return
+	}
+
+	tresp := gjson.ParseBytes(raw)
+	if !tresp.Get("success").Bool() {
+		return info, errors.New("inactive error")
+	} else {
+		return true, nil
 	}
 }
 
